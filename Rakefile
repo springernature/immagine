1d8a4feb924cf542335ad63826ea49f67b507546
Andrea Franz <andrea@gravityblast.com>
2014-04-04 17:12:22 +0100

    updated features

--- Rakefile
+++ Rakefile
@@ -1,5 +1,7 @@
 require 'rspec/core/rake_task'
 
-RSpec::Core::RakeTask.new(:spec)
+RSpec::Core::RakeTask.new(:spec) do |t|
+  t.pattern = "{spec,features}/**/*_spec.rb"
+end
 
 task :default => :spec
--- features/end_to_end_spec.rb
+++ features/end_to_end_spec.rb
@@ -1,40 +1,39 @@
-require 'rack/test'
-require 'minitest/autorun'
+require "spec_helper"
+require "rack/test"
 
 include Rack::Test::Methods
 
 def app
-  rackup_file = File.join(File.dirname(__FILE__), "..", "config.ru")
-  Rack::Builder.parse_file(rackup_file).first
+  ImageResizer::App
 end
 
 describe "Requesting an image" do
   describe "When an invalid url is request" do
     it "returns a 404" do
       get '/wrong_url'
-      last_response.status.must_equal 404
+      expect(last_response.status).to eq(404)
     end
   end
 
   describe "When the format code is not in the whitelist" do
     it "returns a 404" do
       get "/foo/wrong_format/bar.jpg"
-      last_response.status.must_equal 404
+      expect(last_response.status).to eq(404)
     end
   end
 
   describe "When the source image does not exist" do
     it "returns a 404" do
       get "/foo/w300/bar.jpg"
-      last_response.status.must_equal 404
+      expect(last_response.status).to eq(404)
     end
   end
 
   describe "When the format code is valid" do
     it "returns a 200" do
-      app.settings.size_whitelist.each do |f|
+      ImageResizer.settings["size_whitelist"].each do |f|
         get "/images/#{f}/matz.jpg"
-        last_response.status.must_equal 200, "#{f} failed, status is #{last_response.status}"
+        expect(last_response.status).to eq(200)
       end
     end
   end
