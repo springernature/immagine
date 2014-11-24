46368a900e2a65114eaeaac100f31c68589407ef
Darren Oakley <daz.oakley@gmail.com>
2014-11-24 16:58:36 +0000

    Small tweaks
    * Make the sinatra app serve static (non-resized) images - this is so 
      we can have more control over the response headers.
    * Set the ‘Cache-Control’ headers differently for live and staging 
      content.
    * Allow the ‘Cache-Control’ headers to be overridable.
    * Set the ‘Last-Modified’ header with the file’s mtime.
    * Make the image quality overridable via HTTP headers.

--- config.ru
+++ config.ru
@@ -2,11 +2,9 @@ $LOAD_PATH.unshift File.join(File.dirname(__FILE__), 'lib')
 
 env = ENV['RACK_ENV'] || 'development'
 
-require 'rack/contrib/try_static'
 require 'image_resizer'
 
 ImageResizer.init(env)
 
 use Rack::CommonLogger, ImageResizer.logger
-use Rack::TryStatic, urls: [''], root: ImageResizer.settings['source_folder']
 run ImageResizer::Service.new
--- spec/image_resizer/service_spec.rb
+++ spec/image_resizer/service_spec.rb
@@ -22,7 +22,113 @@ describe ImageResizer::Service do
     end
   end
 
-  describe 'Requesting an image' do
+  describe 'Requesting an original (non-resized) image' do
+    context 'when the file exists' do
+      it 'returns a 200 response' do
+        get '/live/images/kitten.jpg'
+        expect(last_response.status).to eq(200)
+      end
+
+      context 'Last-Modified HTTP headers' do
+        it 'sets them' do
+          get '/live/images/kitten.jpg'
+          expect(last_response.header['Last-Modified']).to_not be_nil
+        end
+      end
+
+      context 'ETAGS' do
+        context 'when the file has not changed between requests' do
+          it 'responds with the same Etags' do
+            get '/live/images/kitten.jpg'
+            expect(last_response).to be_ok
+
+            first_etag = last_response.header['ETag']
+
+            get '/live/images/kitten.jpg'
+            expect(last_response).to be_ok
+            expect(last_response.header['ETag']).to eq(first_etag)
+          end
+        end
+
+        context 'when the file HAS changed between requests' do
+          it 'responds with different Etags' do
+            # there are 4 returns here as #send_file checks the mtime too
+            expect(File)
+              .to receive(:mtime)
+              .and_return(Time.utc(2014, 1, 1), Time.utc(2014, 1, 1), Time.utc(2014, 1, 2), Time.utc(2014, 1, 2))
+
+            get '/live/images/kitten.jpg'
+            expect(last_response).to be_ok
+
+            first_etag = last_response.header['ETag']
+
+            get '/live/images/kitten.jpg'
+            expect(last_response).to be_ok
+            expect(last_response.header['ETag']).to_not eq(first_etag)
+          end
+        end
+      end
+
+      context 'Cache-Control' do
+        context 'when a X-Cache-Control HTTP header HAS been passed' do
+          it 'sets the cache control headers accordingly' do
+            get "/live/images/kitten.jpg", {}, { 'HTTP_X_CACHE_CONTROL' => 'private, max-age=60' }
+
+            expect(last_response).to be_ok
+            expect(last_response.header['Cache-Control']).to eq('private, max-age=60')
+          end
+        end
+
+        context 'when a X-Cache-Control HTTP header HAS NOT been passed' do
+          context 'when the image requested is a LIVE asset' do
+            it 'sets the Cache-Control as PUBLIC' do
+              get "/live/images/kitten.jpg"
+
+              expect(last_response).to be_ok
+              expect(last_response.header['Cache-Control']).to include('public')
+            end
+
+            it 'sets the Max-Age as 1 day' do
+              get "/live/images/kitten.jpg"
+
+              expect(last_response).to be_ok
+              expect(last_response.header['Cache-Control']).to include('max-age=86400')
+            end
+          end
+
+          context 'when the image requested is a STAGING asset' do
+            it 'sets the Cache-Control as PRIVATE' do
+              get "/staging/images/kitten.jpg"
+
+              expect(last_response).to be_ok
+              expect(last_response.header['Cache-Control']).to include('private')
+            end
+
+            it 'sets the Max-Age as 0' do
+              get "/staging/images/kitten.jpg"
+
+              expect(last_response).to be_ok
+              expect(last_response.header['Cache-Control']).to include('max-age=0')
+            end
+          end
+        end
+      end
+    end
+
+    context 'when the file does not exist' do
+      it 'returns a 404' do
+        get '/live/images/matzwibble.jpg'
+        expect(last_response.status).to eq(404)
+      end
+
+      it 'does not set cache-control headers' do
+        get '/live/images/matzwibble.jpg'
+        expect(last_response.header['Cache-Control']).to be_nil
+      end
+    end
+  end
+
+  describe 'Requesting a resized image' do
     context 'with an invaid URL (no dir)' do
       it 'returns a 404' do
         get '/live/bar.jpg'
@@ -53,6 +159,13 @@ describe ImageResizer::Service do
       end
     end
 
+    context 'Last-Modified HTTP headers' do
+      it 'sets them' do
+        get "/live/images/#{ImageResizer.settings['size_whitelist'].sample}/kitten.jpg"
+        expect(last_response.header['Last-Modified']).to_not be_nil
+      end
+    end
+
     context 'ETAGS' do
       let(:format_code) { ImageResizer.settings['size_whitelist'].sample }
 
@@ -71,9 +184,10 @@ describe ImageResizer::Service do
 
       context 'when the file HAS been modified between requests' do
         it 'should return DIFFERENT ETAGs' do
+          # called twice per request (for etags and last_modified)
           expect(File)
             .to receive(:mtime)
-            .and_return(Time.utc(2014, 1, 1), Time.utc(2014, 1, 2))
+            .and_return(Time.utc(2014, 1, 1), Time.utc(2014, 1, 1), Time.utc(2014, 1, 2), Time.utc(2014, 1, 2))
 
           get "/live/images/#{format_code}/kitten.jpg"
           expect(last_response).to be_ok
@@ -84,6 +198,63 @@ describe ImageResizer::Service do
           expect(last_response).to be_ok
           expect(last_response.header['ETag']).to_not eq(first_etag)
         end
+      end
+    end
+
+    context 'Cache-Control' do
+      let(:format_code) { ImageResizer.settings['size_whitelist'].sample }
+
+      context 'when a X-Cache-Control HTTP header HAS been passed' do
+        it 'sets the cache control headers accordingly' do
+          get "/live/images/#{format_code}/kitten.jpg", {}, { 'HTTP_X_CACHE_CONTROL' => 'private, max-age=60' }
+
+          expect(last_response).to be_ok
+          expect(last_response.header['Cache-Control']).to eq('private, max-age=60')
+        end
+      end
+
+      context 'when a X-Cache-Control HTTP header HAS NOT been passed' do
+        context 'when the image requested is a LIVE asset' do
+          it 'sets the Cache-Control as PUBLIC' do
+            get "/live/images/#{format_code}/kitten.jpg"
+
+            expect(last_response).to be_ok
+            expect(last_response.header['Cache-Control']).to include('public')
+          end
+
+          it 'sets the Max-Age as 1 day' do
+            get "/live/images/#{format_code}/kitten.jpg"
+
+            expect(last_response).to be_ok
+            expect(last_response.header['Cache-Control']).to include('max-age=86400')
+          end
+        end
+
+        context 'when the image requested is a STAGING asset' do
+          it 'sets the Cache-Control as PRIVATE' do
+            get "/staging/images/#{format_code}/kitten.jpg"
+
+            expect(last_response).to be_ok
+            expect(last_response.header['Cache-Control']).to include('private')
+          end
+
+          it 'sets the Max-Age as 0' do
+            get "/staging/images/#{format_code}/kitten.jpg"
+
+            expect(last_response).to be_ok
+            expect(last_response.header['Cache-Control']).to include('max-age=0')
+          end
+        end
+      end
+    end
+
+    context 'Image Quality' do
+      context 'no custom headers' do
+        it 'uses the default image quality setting'
+      end
+
+      context 'with a X-Image-Quality HTTP header' do
+        it 'uses the passed quality setting'
       end
     end
   end
